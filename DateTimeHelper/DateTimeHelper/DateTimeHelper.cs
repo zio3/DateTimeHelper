d82bc2cec2417b0b7bfdf995b5e053e4954682c5
zio3 <info@zio3.net>
2015-02-06 22:24:22 +0900

    公開ライブラリにするためGitHubに移行

---
+++ DateTimeHelper/DateTimeHelper/DateTimeHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class DateTimeHelper
+    {
+        static private readonly TimeZoneInfo jstZoneInfo = System.TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+
+        static public DateTime Now
+        {
+            get
+            {
+                var utdDate = DateTime.UtcNow;
+                DateTime jst = System.TimeZoneInfo.ConvertTimeFromUtc(utdDate, jstZoneInfo);
+                return jst;
+            }
+        }
+
+        static public DateTime Today
+        {
+            get
+            {
+                return DateTimeHelper.Now.Date;
+            }
+        }
+        static public IEnumerable<DateTime> EnumDateTime(DateTime start, DateTime end, TimeSpan step, bool lastWith = false)
+        {
+            var current = start;
+            while (current < end)
+            {
+                yield return current;
+                current = current.Add(step);
+            }
+
+            if (lastWith)
+                yield return end;
+        }
+
+        static public DateTime ToMonthStart(DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+
+        static public IEnumerable<DateTime> EnumMonthDateTime(DateTime start, DateTime end, bool lastWith = false)
+        {
+            var current = ToMonthStart(start);
+            end = ToMonthStart(end);
+            while (current < end)
+            {
+                yield return current;
+                current = current.AddMonths(1);
+            }
+
+            if (lastWith)
+                yield return end;
+        }
+    }
+}
